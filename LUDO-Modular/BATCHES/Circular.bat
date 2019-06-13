fac5bf9326423d8e9a854aa3e21be7388bcf2ae1
Esduard <42816427+Esduard@users.noreply.github.com>
2019-06-13 19:58:35 -0300

    adicionados bat circular e peca

---
+++ LUDO-Modular/BATCHES/Circular.bat
@@ -0,0 +1,10 @@
+REM Recompila lista circular
+
+cls
+
+Call LimpaTudo.bat
+Call GeraTudo.bat
+Call Compila TesteCircular
+Call Testa TesteCircular TesteCircular
+
+:sai
---
+++ LUDO-Modular/BATCHES/peca.bat
@@ -0,0 +1,10 @@
+REM Recompila peca
+
+cls
+
+Call LimpaTudo.bat
+Call GeraTudo.bat
+Call Compila TestePeca
+Call Testa TestePeca TestePeca
+
+:sai
