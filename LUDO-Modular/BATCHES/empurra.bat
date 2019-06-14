d6a2b4bc7ac884311af5d4c56e0535352f1018e9
Esduard <42816427+Esduard@users.noreply.github.com>
2019-06-13 22:45:42 -0300

    bat_de_commit

---
+++ LUDO-Modular/BATCHES/empurra.bat
@@ -0,0 +1,21 @@
+@ECHO  OFF
+REM  envia para Repositorio com mensagem escrita
+REM  
+              
+
+pushd  .
+
+if ""=="%1" goto erro
+
+call LimpaTudo.bat
+git add --all
+git commit -m "%1"
+git push
+
+goto sai
+
+:erro
+echo Falta mensagem do commit
+
+:sai
+popd
