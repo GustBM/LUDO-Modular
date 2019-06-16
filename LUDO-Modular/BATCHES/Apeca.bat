dcfe9b476001b234745636092c325b0a954922a5
Esduard <42816427+Esduard@users.noreply.github.com>
2019-06-15 21:41:36 -0300

    Apeca_ok

--- LUDO-Modular/BATCHES/Apeca.bat
+++ LUDO-Modular/BATCHES/Apeca.bat
@@ -4,7 +4,7 @@ cls
 
 call vsvars32
 Call LimpaTudo.bat
-gmake  /B..\PRODUTOS\ /c..\COMPOSICAO\TestePeca     /P..\COMPOSICAO\Gmake.parm
+Call GeraTudo.bat
 Call Compila TestePeca
 Call Testa TestePeca TestePeca
 
